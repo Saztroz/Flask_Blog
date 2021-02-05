8d03d4536e535a001f82808b0141763c8f02207e
Sain <hussein.rahal11@gmail.com>
2021-02-04 19:48:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flask_Blog
+ Created Blog using Flask
---
+++ flaskblog/config.py
@@ -0,0 +1,28 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = '6bce2ccb2d938fc74959256c0f2a1b13'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# db = SQLAlchemy(app)
+# bcrypt = Bcrypt(app)
+# login_manager = LoginManager(app)
+# login_manager.login_view = 'login'
+# login_manager.login_message_category = 'info'
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+# app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+# mail = Mail(app)
+
+# from flaskblog import routes
